93e10786c046465c042bff064832b05b079adcaa
Bruna S. Pires <69404377+piresbruna@users.noreply.github.com>
2022-06-21 21:44:08 -0300

    Commit inicial do projeto de estudo de testes.

---
+++ src/main/java/studyjavatest/App.java
@@ -0,0 +1,13 @@
+package studyjavatest;
+
+public class App {
+  public String getGreeting(){
+    return "Hello World!";
+  }
+
+  public static void main(String[] args) {
+    App app = new App();
+    System.out.println(app.getGreeting());
+  }
+  
+}
---
+++ src/test/java/studyjavatest/AppTest.java
@@ -0,0 +1,14 @@
+package studyjavatest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+  @Test
+  public void testAppGreting(){
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+  
+}
